1d66c60cea5e749e4b3ee41b705a4b0b7bb01041
片野良祐 <katano_ryousuke@katanoryousukenoMacBook-Air.local>
2019-07-25 19:47:25 +0900

    submit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,8 @@ class BooksController < ApplicationController
   def create
    book = Book.new(book_params)
    book.save
-   redirect_to '/books/:id'
+   flash[:notice] = "Book was successfully created."
+   redirect_to book_path(book)
   end
 
   def edit
@@ -24,7 +25,14 @@ class BooksController < ApplicationController
   def update
    book = Book.find(params[:id])
    book.update(book_params)
+   flash[:notice] = "Book was successfully updated."
    redirect_to book_path(book)
+  end
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
   end
 
   private
